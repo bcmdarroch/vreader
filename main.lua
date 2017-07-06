70f791f829552bdf1226887b517daa914283f264
Brenna <bcmdarroch@gmail.com>
2017-07-06 16:34:22 -0700

    testing placing environment

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ function lovr.load()
   -- load book/text
 
   -- load model
-  -- environment = lovr.graphics.newModel(/assets/'')
+  environment = lovr.graphics.newModel('assets/Room_block.fbx')
 
   -- load audio
 
@@ -20,9 +20,15 @@ function lovr.update()
 end
 
 function lovr.draw()
-  lovr.graphics.cube('line', 0, 0, -2)
-  lovr.graphics.print("hello world!", 0, 0, -5)
+  -- test
+  -- lovr.graphics.cube('line', 0, 0, -2)
+  -- lovr.graphics.print("hello world!", 0, 0, -5)
+
   -- render environment given user's position in space
+  -- environment:draw(0, 0, 0, 1, 90)
+  environment:draw(0, 0, 0, 4, 90)
+
+  lovr.graphics.setBackgroundColor(0, 0, 255, 255)
 
   -- if read mode on, render page with in front of camera
 
