1154632f981be768c3e583d28351de19082c0d62
Brenna <bcmdarroch@gmail.com>
2017-07-05 20:41:06 -0700

    added placeholder environment

---
+++ main.lua
@@ -0,0 +1,28 @@
+function lovr.load()
+  -- load book/text
+
+  -- load model
+
+  -- load audio
+
+  -- load controllers
+
+  -- load page (plane)
+
+end
+
+function lovr.update()
+  -- has user moved?
+
+  -- has user
+
+end
+
+function lovr.draw()
+  -- render environment given user's position in space
+
+  -- if read mode on, render page with in front of camera
+
+  -- render UI
+
+end
