f204146e1caed778136dc47fae555e047dbd57b7
Brenna <bcmdarroch@gmail.com>
2017-07-06 14:29:28 -0700

    tested code in lovr:

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ function lovr.load()
   -- load book/text
 
   -- load model
+  -- environment = lovr.graphics.newModel(/assets/'')
 
   -- load audio
 
@@ -19,6 +20,8 @@ function lovr.update()
 end
 
 function lovr.draw()
+  lovr.graphics.cube('line', 0, 0, -2)
+  lovr.graphics.print("hello world!", 0, 0, -5)
   -- render environment given user's position in space
 
   -- if read mode on, render page with in front of camera
