c65db090792bd5e7e0b81c78005bb9f44abe1d97
Brenna <bcmdarroch@gmail.com>
2017-07-10 14:18:56 -0700

    added lib folder, wrote printText and helper functions

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 -- LOVR callbacks
 function lovr.load()
-  -- require('functions')
-  require('test')
+  require('lib/lua_lib')
+  require('lib/lovr_lib')
+
   width, height = lovr.graphics.getDimensions()
 
   -- load model
@@ -36,11 +37,15 @@ end
 
 
 function lovr.draw()
+  -- mac testing:
+  lovr.graphics.plane('line', 0, 0, -2, 1, 0, 0, 1)
+  lovr.graphics.print(lovr.printText(displayText, 1, 100), 0, 0, -2, 0.05, 0, 0, 0, 0, 10, left, top)
+
   -- play background sound
   sound:play()
 
   -- render environment given user's position in space
-  environment:draw(0, 0, 0, .4)
+  -- environment:draw(0, 0, 0, .4)
 
   -- if read mode on, render page with in front of camera
   lovr.graphics.plane('line', 0, 1, 0, 1, 0, 0, 1)
@@ -49,26 +54,11 @@ function lovr.draw()
   -- lovr.graphics.setShader(font) -- setShader/setFont doesn't work
   -- font:setPixelDensity(50)
   -- lovr.graphics.setColor(0, 0, 0, 255)
-  lovr.graphics.print(lovr.printText(displayText), 0, 1, 0, 0.05, 0, 0, 0, 0, 10, left, top)
-  -- lovr.graphics.print(str, x, y, z, scale, angle, ax, ay, az, wrap, halign, valign)
+  lovr.graphics.print(lovr.printText(displayText, 1, 60), 0, 1, 0, 0.05, 0, 0, 0, 0, 10, left, top)
 
   -- render UI
   for i, controller in pairs(controllers) do
     local x, y, z = controller:getPosition()
     lovr.graphics.cube('line', x, y, z, 0.1, controller:getOrientation())
   end
-end
-
-function lovr.printText(fullText)
-  -- iterate over string, end string once 60 spaces counted
-  local wordCount = 0
-  for word in fullText:gmatch("%f[%w]") do
-    -- while (wordCount < 500 or word ~= " ") do
-    local wordCount = 0
-    while wordCount < 500 do
-      wordCount = wordCount + 1
-    end
-    return string.sub(fullText, 1, wordCount)
-  end
-  -- load 60 words at a time
 end
