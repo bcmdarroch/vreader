45d89860d2dbee2be7040e5ddbfda39af28d03ce
Brenna Darroch <bcmdarroch@gmail.com>
2017-07-13 13:12:57 -0700

    added book

---
+++ lib/book.lua
@@ -0,0 +1,24 @@
+-- book load
+
+-- book update
+
+-- book draw
+
+-- prints section of text
+function lovr.printText(fullText, start, numWords)
+  -- split text by word into table
+  local words = {}
+  for word in fullText:gmatch("%S+") do
+    -- count new lines!
+    table.insert(words, word)
+  end
+
+  -- adjust finish variable if needed
+  local finish = start + numWords
+  if finish > tableLength(words) then
+    finish = tableLength(words)
+  end
+
+  -- return section of text
+  return table.concat(words, " ", start, finish)
+end
