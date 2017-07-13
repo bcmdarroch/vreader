253739db2bf2e4cbe056bdd8cabd0bb82510bf15
Brenna <bcmdarroch@gmail.com>
2017-07-13 11:54:33 -0700

    pseudocode moving page

--- main.lua
+++ main.lua
@@ -28,58 +28,78 @@ end
 
 function lovr.update(dt)
   -- test
-  -- START = START + NUMCHARS
-  -- print(START)
-  -- print(NUMCHARS)
+  START = START + NUMCHARS * dt * .05
+  print(START)
+  print(NUMCHARS)
 
   -- has user clicked read mode? (menu button)
-  for i, controller in ipairs(controllers) do
-    if controller:isDown('menu') then
-      READMODE = not READMODE
-    end
-  end
+  -- for i, controller in ipairs(controllers) do
+  --   if controller:isDown('menu') then
+  --     READMODE = not READMODE
+  --   end
+  -- end
 
   -- has user clicked next page?
   if READMODE then
     for i, controller in ipairs(controllers) do
       -- clicking touchpad flips page forward
       if controller:isDown('touchpad') then
-        START = START + 1
+        START = START + 1 -- should be: go to next line
+        -- change alpha of plane to make it fade
         if START > string.len(displayText) then
           START = string.len(displayText) - NUMCHARS
         end
       -- clicking trigger flips page backward
       elseif controller:getAxis('trigger') == 1 then
-        START = START - 1
+        START = START - 1 -- should be: go back a line
         if START < 1 then
           START = 1
         end
       end
     end
 
-    -- if controller:getAxis('touchx') == 1 then
-    -- elseif controller:getAxis('touchy') == 1 then
-  end
+  -- if controller pressed (touchpad)
+      -- if controller:getAxis('touchx') == 1
+      print(controller:getAxis('touchx'))
+      -- 1, go forward
+      -- elseif controller:getAxis('touchx') == 0.51 then
+      print(controller:getAxis('touchx'))
+  -- end
+
+  -- if controller:getAxis('trigger') == 1 and controller colliding with plane collider (wider than plane)
+    -- change origin of plane/text
+  -- end
+
+
+  -- lovr.controllerPlaneCollide
+  -- give plane collider, controller sphere
+  -- get controller position
+  -- get distance btwn plane and controller origins (position - position)
+  -- if distance < CONSTANT
+    -- return true
+  -- else
+    -- return false
+
 
 end
 
 
 function lovr.draw()
   -- mac testing:
-  -- lovr.graphics.plane('line', 0, 0, -1, 1, 0, 0, 1)
-  -- lovr.graphics.print(lovr.printText(displayText, START, NUMCHARS), 0, 0, -1, 0.05, 0, 0, 0, 0, 15, left, top)
+  lovr.graphics.plane('line', 0, 0, -1, 1, 0, 0, 1)
+  lovr.graphics.print(lovr.printText(displayText, START, NUMCHARS), 0, 0, -1, 0.05, 0, 0, 0, 0, 15, left, top)
 
   -- play background sound
   sound:play()
 
   -- render environment given user's position in space
-  environment:draw(0, 0, 0, .4)
+  -- environment:draw(0, 0, 0, .4)
 
   -- render UI
-  for i, controller in ipairs(controllers) do
-   x, y, z = controller:getPosition()
-   angle, ax, ay, az = controller:getOrientation()
-   controllerModels[i]:draw(x, y, z, 1, angle, ax, ay, az)
+  -- for i, controller in ipairs(controllers) do
+  --  x, y, z = controller:getPosition()
+  --  angle, ax, ay, az = controller:getOrientation()
+  --  controllerModels[i]:draw(x, y, z, 1, angle, ax, ay, az)
  end
 
   -- if read mode on, render page with in front of camera
