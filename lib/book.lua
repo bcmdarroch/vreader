4d45e96be2c920d7aaa4f50936db5650ad63e346
Brenna <bcmdarroch@gmail.com>
2017-07-18 11:43:47 -0700

    testing touchy page turn, turnPage function in book, inverse text display

--- lib/book.lua
+++ lib/book.lua
@@ -5,25 +5,49 @@ function Book:init(title, author, text)
   self.title = title
   self.author = author
   self.text = Book:parseTxt(text)
+  self.inverse = false
   return self
 
 end
 
 -- book update
 function Book:update()
+  for i, controller in ipairs(controllers) do
+    -- if trigger down and controller within book plane
+    if controller:getAxis('trigger') == 1 and lovr.controllerPlaneCollide(controller) == true then
+      -- change book position
+      -- BX, BY, BZ = controller:getPosition()
 
+      -- first implementation:
+      -- translate rotation (keep forward vector of fixed toward headset)
+      -- angle, BAX, BAY, BAZ = controller:getOrientation()
+      -- lovr.graphics.rotate(angle, BAX, BAY, BAZ)
+      -- transform = lovr.math.newTransform(BX, BY, BZ, 0, 0, 0, angle, BAX, BAY, BAZ)
+
+      -- second implementation (to draw):
+       rotateMode = true
+    end
+  end
 
 end
 
 -- book draw
 function Book:draw(p, mode, x, y, z, planeSize, nx, ny, nz, textScale, textAngle, ax, ay, az)
+  -- render plane
+  -- if self.inverse then
+    lovr.graphics.setColor(0, 0, 0)
+  -- end
   lovr.graphics.plane(mode, x, y, z, planeSize, nx, ny, nz)
 
+  -- render text
+  -- if self.inverse then
+    lovr.graphics.setColor(255, 255, 255)
+  -- end
   -- lovr.graphics.setShader(font) -- setShader/setFont doesn't work
   -- font:setPixelDensity(50)
-  -- lovr.graphics.setColor(0, 0, 0, 255)
-  lovr.graphics.print(self.text[p], x, y, z, textScale, textAngle, ax, ay, az, 10, left, top)
+  lovr.graphics.print(self.text[p], x, y - 0.02, z, textScale, textAngle, ax, ay, az, 12, left, top)
   lovr.graphics.print(p, x + 0.45, y - 0.45, z, textScale - 0.02, textAngle, ax, ay, az, 10, left, top)
+  lovr.graphics.print(self.title, x, y + 0.45, z, textScale - 0.02, textAngle, ax, ay, az, 10, left, top)
 
 end
 
@@ -38,12 +62,11 @@ function Book:parseTxt(text)
     local char = string.sub(text, j, j)
     local next = string.sub(text, j + 1, j + 1)
 
-    -- 1. end of word
-    -- 2. in btwn words
+    -- check if ends at end of word, in btwn words
     if char == " " or next == " " then
       table.insert(textTable, string.sub(text, i, j))
 
-    -- 3. inside word
+    -- check if ends inside word
     elseif char ~= " " and next ~= " " then
       local prev = ""
       repeat
@@ -57,9 +80,27 @@ function Book:parseTxt(text)
     i = j
     j = i + 500
   end
-
   return textTable
 
+end
+
+function Book:turnPage(controller)
+  if controller:getAxis('touchx') > 0 or controller:getAxis('touchy') > 0 then
+      PAGE = PAGE + 1
+      if PAGE > #book.text then
+        PAGE = #book.text
+      end
+  elseif controller:getAxis('touchx') < 0 and controller:getAxis('touchy') < 0 then
+    PAGE = PAGE - 1
+    if PAGE < 1 then
+      PAGE = 1
+    end
+  end
+
+end
+
+function Book:inverseColors()
+  self.inverse = not self.inverse
 end
 
 return Book
--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
--- main LOVR callbacks
+-- 1. main LOVR callbacks
 function lovr.load()
   require('lib/book')
   require('lib/lua_lib')
 
-  -- load model
+  -- load environment
   environment =  lovr.graphics.newModel('assets/Room_block_small.obj', 'assets/texture.jpg')
 
   -- load audio
@@ -13,16 +13,10 @@ function lovr.load()
   -- load controllers
   refreshControllers()
 
-  -- load book/text (without book class)
-  START = 1
-  NUMWORDS = 100
-  displayText = lovr.filesystem.read('assets/room/part1.txt')
-  -- displayText = lovr.filesystem.read('assets/room/part1.txt') .. lovr.filesystem.read('assets/room/part2.txt') .. lovr.filesystem.read('assets/room/part3.txt') .. lovr.filesystem.read('assets/room/part4.txt') .. lovr.filesystem.read('assets/room/part5.txt') .. lovr.filesystem.read('assets/room/part6.txt')
-  -- font = lovr.graphics.newFont('assets/Arvo-Regular.ttf', '20')
-
-  -- load book w book class
-  bookText = lovr.filesystem.read('assets/room/part1.txt')
+  -- load book
+  bookText = lovr.filesystem.read('assets/room/part1.txt') .. lovr.filesystem.read('assets/room/part2.txt') .. lovr.filesystem.read('assets/room/part3.txt') .. lovr.filesystem.read('assets/room/part4.txt') .. lovr.filesystem.read('assets/room/part5.txt') .. lovr.filesystem.read('assets/room/part6.txt')
   book = Book:init("A Room of One's Own", "Virginia Woolf", bookText)
+  -- font = lovr.graphics.newFont('assets/Arvo-Regular.ttf', '20')
 
   PAGE = 1
   BX, BY, BZ = 0, 1, 0
@@ -39,30 +33,13 @@ function lovr.load()
 end
 
 function lovr.update()
-  for i, controller in ipairs(controllers) do
-    -- if trigger down and controller within book plane
-    if controller:getAxis('trigger') == 1 and lovr.controllerPlaneCollide(controller) == true then
-      -- change book position
-      -- BX, BY, BZ = controller:getPosition()
-
-      -- first implementation:
-      -- translate rotation (keep forward vector of fixed toward headset)
-      -- angle, BAX, BAY, BAZ = controller:getOrientation()
-      -- lovr.graphics.rotate(angle, BAX, BAY, BAZ)
-      -- transform = lovr.math.newTransform(BX, BY, BZ, 0, 0, 0, angle, BAX, BAY, BAZ)
-
-      -- second implementation (to draw): 
-       rotateMode = true
-    end
-  end
+  book:update()
 
 end
 
 function lovr.draw()
   -- mac testing:
-  -- lovr.graphics.plane('line', 0, 0, -1, 1, 0, 0, 1)
-  -- lovr.graphics.print(lovr.printText(displayText, START, NUMWORDS), 0, 0, -1, 0.05, 0, 0, 0, 0, 15, left, top)
-  -- book:draw('line', 0, 0, -1, planeSize, NX, NY, NZ, textScale, angle, BAX, BAY, BAZ)
+  book:draw(PAGE, 'fill', 0, 0, -1, planeSize, NX, NY, NZ, textScale, angle, BAX, BAY, BAZ)
 
   -- origin
   -- lovr.graphics.sphere(0, 0, 0, .1, 0, 0, 1)
@@ -71,17 +48,13 @@ function lovr.draw()
   -- sound:play()
 
   -- render environment given user's position in space
-  environment:draw(0, 0, 0, .4)
+  -- environment:draw(0, 0, 0, .4)
 
   -- render UI
-  for i, controller in ipairs(controllers) do
-   x, y, z = controller:getPosition()
-   angle, ax, ay, az = controller:getOrientation()
-   controllerModels[i]:draw(x, y, z, 1, angle, ax, ay, az)
-  end
+  renderControllers()
 
   -- render book
-  book:draw(PAGE, 'line', BX, BY, BZ, planeSize, NX, NY, NZ, textScale, angle, BAX, BAY, BAZ)
+  book:draw(PAGE, 'fill', BX, BY, BZ, planeSize, NX, NY, NZ, textScale, angle, BAX, BAY, BAZ)
 
   -- second: artisanal hand-crafted rotation
   if rotateMode then
@@ -98,13 +71,21 @@ function lovr.draw()
 
 end
 
-
--- controller functions
+-- 2. controller functions
 function refreshControllers()
   controllers = lovr.headset.getControllers()
   controllerModels = {}
   for i, controller in ipairs(controllers) do
     controllerModels[i] = controller:newModel()
+  end
+
+end
+
+function renderControllers()
+  for i, controller in ipairs(controllers) do
+   x, y, z = controller:getPosition()
+   angle, ax, ay, az = controller:getOrientation()
+   controllerModels[i]:draw(x, y, z, 1, angle, ax, ay, az)
   end
 
 end
@@ -120,25 +101,17 @@ function lovr.controllerremoved()
 end
 
 function lovr.controllerpressed(controller, button)
-
   if button == 'touchpad' then
-    if controller:getAxis('touchx') > 0 then
-        PAGE = PAGE + 1
-        if PAGE > #book.text then
-          PAGE = #book.text
-        end
-    elseif controller:getAxis('touchx') < 0 then
-      PAGE = PAGE - 1
-      if PAGE < 1 then
-        PAGE = 1
-      end
-    end
+    book:turnPage(controller)
+  end
+
+  if button == 'grip' then
+    book:inverseColors()
   end
 
 end
 
-
--- control book position
+-- check controller position relative to book
 function lovr.controllerPlaneCollide(controller)
   -- get controller position
   conX, conY, conZ = controller:getPosition()
