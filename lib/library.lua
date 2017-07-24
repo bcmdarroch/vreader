1fe535da8dc6d442f1f787a8a5878754c5b289e5
Brenna Darroch <bcmdarroch@gmail.com>
2017-07-24 16:41:01 -0700

    changed book positions to transforms

---
+++ lib/library.lua
@@ -0,0 +1,110 @@
+Object = require("lib/classic")
+
+Library = Object:extend()
+
+function Library:init()
+  self.books = {}
+  -- self.books['test'] = 'blahhhh'
+
+  return self
+
+end
+
+function Library:load()
+  local files = lovr.filesystem.getDirectoryItems('assets/books/room/')
+  local roomText = ""
+  for i, file in ipairs(files) do
+    roomText = roomText .. lovr.filesystem.read('assets/books/room/' .. file)
+  end
+
+  room = {
+    book = Book:init("A Room of One's Own", "Virginia Woolf", roomText),
+    model = lovr.graphics.newModel('assets/models/book.obj', 'assets/textures/texture.jpg'),
+    position = lovr.math.newTransform(0, 0, 0, 0.3)
+  }
+  -- self.books['Room'] = room
+  table.insert(self.books, room)
+
+  prince = {
+    book = Book:init("The Prince", "Nicolo Machiavelli", Library:getText('assets/books/unzipped/Prince')),
+    model = lovr.graphics.newModel('assets/models/book.obj', 'assets/textures/texture.jpg'),
+    position = lovr.math.newTransform(0, 1, 0, 0.3)
+  }
+  -- self.books['Prince'] = prince
+  table.insert(self.books, prince)
+
+  emma = {
+    book = Book:init("Emma", "Jane Austen", Library:getText('assets/books/unzipped/Emma')),
+    model = lovr.graphics.newModel('assets/models/book.obj', 'assets/textures/texture.jpg'),
+    position = lovr.math.newTransform(0, 0, 0, 0.3)
+  }
+  -- self.books['Emma'] = emma
+  table.insert(self.books, emma)
+
+end
+
+-- load files
+function Library:getText(dir)
+  local files = lovr.filesystem.getDirectoryItems(dir)
+  local text = ""
+
+  -- iterate over HTML files from unzipped epub
+  for i, file in ipairs(files) do
+  --  local html = lovr.filesystem.read(dir .. '/' .. file)
+  --  print('html', html)
+   local extract = Library:parseHTML(dir .. '/' .. file)
+   text = text .. extract
+  end
+  return text
+
+end
+
+-- extract text
+function Library:parseHTML(file)
+  local htmlparser = require('lib/htmlparser/init')
+
+  local rawText = lovr.filesystem.read(file)
+  local html = htmlparser.parse(rawText)
+
+  -- select all paragraphs except for Table of Contents
+  local elements = html:select("p:not(.toc)")
+
+  local text = ""
+  for _, e in ipairs(elements) do
+    -- extract p-tag content
+    text = text .. "\n\n" .. e:getcontent()
+  end
+
+  -- strip HTML word styling
+  text = string.gsub(text, "<(.)>", "")
+  text = string.gsub(text, "<(.+)/>", "")
+  text = string.gsub(text, "</(.)>", "")
+
+  -- print("parseHTML returned text", string.sub(text, 1, 500))
+  return text
+
+end
+
+-- places all books in environment
+function Library:draw()
+  -- local x = 0
+  -- local y = 0
+  -- local z = 0
+
+  for i, book in ipairs(self.books) do
+    local x, y, z = book['position']
+    book['model']:draw(x, y, z, 0.4)
+    -- x = x + 0.5
+    -- y = y + 0.5
+    -- z = z + 0.5
+  end
+
+end
+
+-- checks if a book is active or not
+function Library:setActiveBook()
+
+
+end
+
+return Library
