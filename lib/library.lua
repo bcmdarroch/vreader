6a407be39568a72c7b018870671f16792843e0ee
Brenna Darroch <bcmdarroch@gmail.com>
2017-07-26 10:09:33 -0700

    merged epub w master

---
+++ lib/library.lua
@@ -0,0 +1,92 @@
+Object = require("lib/classic")
+
+Library = Object:extend()
+
+function Library:new()
+  self.books = {}
+
+end
+
+function Library:load()
+  local files = lovr.filesystem.getDirectoryItems('assets/books/room/')
+  local roomText = ""
+  for i, file in ipairs(files) do
+    roomText = roomText .. lovr.filesystem.read('assets/books/room/' .. file)
+  end
+
+  room = {
+    book = Book("A Room of One's Own", "Virginia Woolf", roomText),
+    model = lovr.graphics.newModel('assets/models/book.obj', 'assets/textures/Room_diffuse.png'),
+    position = lovr.math.newTransform(-0.9, 0.38, 0.4, SCALE, SCALE, SCALE, math.rad(90), 0, 0, 1),
+  }
+  self.books['Room'] = room
+
+  prince = {
+    book = Book("The Prince", "Nicolo Machiavelli", Library:getText('assets/books/unzipped/ThePrince')),
+    model = lovr.graphics.newModel('assets/models/book.obj', 'assets/textures/Prince_diffuse.png'),
+    position = lovr.math.newTransform(0.9, 0.89, -1, SCALE, SCALE, SCALE, math.rad(90), 0, 0, 1),
+  }
+  self.books['Prince'] = prince
+
+  emma = {
+    book = Book("Emma", "Jane Austen", Library:getText('assets/books/unzipped/Emma')),
+    model = lovr.graphics.newModel('assets/models/book.obj', 'assets/textures/Emma_diffuse.png'),
+    position = lovr.math.newTransform(0.2, 0.76, -1.4, SCALE, SCALE, SCALE, math.rad(90), 0, 0, 1),
+  }
+  self.books['Emma'] = emma
+
+end
+
+-- load files
+function Library:getText(dir)
+  local files = lovr.filesystem.getDirectoryItems(dir)
+  local text = ""
+
+  -- iterate over HTML files from unzipped epub
+  for i, file in ipairs(files) do
+   local extract = Library:parseHTML(dir .. '/' .. file)
+   text = text .. extract
+  end
+  return text
+
+end
+
+-- extract text
+function Library:parseHTML(file)
+  local htmlparser = require('lib/htmlparser/init')
+
+  -- remove nested a-tags
+  local rawText = lovr.filesystem.read(file)
+  rawText = string.gsub(rawText, [[<a (.+)<!-- H2 anchor -->]], "")
+  rawText = string.gsub(rawText, "h1", "p")
+  rawText = string.gsub(rawText, "h2", "p")
+
+  -- parse text and select all paragraphs except for Table of Contents
+  local html = htmlparser.parse(rawText)
+  local elements = html:select("p:not(.toc)")
+
+  local text = ""
+  for _, e in ipairs(elements) do
+    -- extract p-tag content
+    text = text .. "\n\n" .. e:getcontent()
+  end
+
+  -- strip HTML word styling
+  text = string.gsub(text, "<(.)>", "")
+  text = string.gsub(text, "<(.+)/>", "")
+  text = string.gsub(text, "</(.)>", "")
+
+  -- print("parseHTML returned text", string.sub(text, 1, 500))
+  return text
+
+end
+
+-- places all books in environment
+function Library:draw()
+  for i, book in pairs(self.books) do
+    book['model']:draw(book['position'])
+  end
+
+end
+
+return Library
---
+++ lib/simple.lua
@@ -0,0 +1,39 @@
+return lovr.graphics.newShader([[
+// Declare two variables that we are going to calculate in the vertex shader and send to the
+// fragment shader.
+out vec3 lightDirection;
+out vec3 normalDirection;
+// This variable defines the position of the light source.  It could be a uniform if you want
+// to change it over time (see commented line).
+vec3 lightPosition = vec3(0, 10, 0);
+// uniform vec3 lightPosition; // use shader:send('lightPosition', { x, y, z }) to update
+vec4 position(mat4 projection, mat4 transform, vec4 vertex) {
+  // Use matrices to transform position vectors and normal vectors.  They are given to us in
+  // "local" space but we want them to be in world space.  We do this by multiplying the vertex
+  // by the "lovrTransform" matrix given to us by LÖVR.
+  vec3 transformedPosition = vec3(lovrTransform * vec4(lovrPosition, 1));
+  vec3 transformedNormal = vec3(lovrTransform * vec4(lovrNormal, 0));
+  // Now that we have everything in world space, we can compute the direction between the vertex
+  // and the light.  We also normalize everything (make the lengths of the vectors 1) to avoid
+  // errors in calculations.
+  lightDirection = normalize(lightPosition - transformedPosition);
+  normalDirection = normalize(transformedNormal);
+  // This is the behavior for the default vertex shader, we leave it here so everything works!
+  return projection * transform * vertex;
+}
+]], [[
+// Declare the two variables that are sent to us by the vertex shader
+in vec3 lightDirection;
+in vec3 normalDirection;
+vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) {
+  // Use the dot product to calculate the intensity of the light.  It will be between 0 and 1 if
+  // the surface is facing the light, or less than zero if the surface is facing away from the
+  // light.
+  vec3 ambient = vec3(.5, .5, .5);
+  float lightIntensity = max(dot(lightDirection, normalDirection), 0);
+  // Calculate the final color for this little pixel!
+  // vec4 lightColor = vec4(vec3(lightIntensity), 1.);
+  vec4 lightColor = vec4(clamp(ambient + vec3(lightIntensity), vec3(0), vec3(1)), 1.);
+  return lightColor * graphicsColor * texture(image, uv);
+}
+]])
